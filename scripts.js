8f931c49394db3364fda608af11444d656bcba68
Feisal Ahmed <feisala132@gmail.com>
2024-10-25 13:20:10 +0100

    Adding break timers

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 let timer;
-let sessions;
+let sessions = 0
 let isRunning = false;
-let breakStart = false
-let timeLeft = 25 * 60;
+
+
+let timeLeft = 25 * 60
 let smallbreakLeft = 5 * 60
 let longbreakLeft = 15 * 60
+
 const timer_display =  document.getElementById('timer-display')
 const Start_btn = document.getElementById('Start_btn')
 const Pause_btn = document.getElementById('Pause_btn')
@@ -19,15 +21,26 @@ function updateDisplay() {
         `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }
 
-function smallbreakDisplay () {
-    const minutes = Math.floor(smallbreakLeft / 60);
-    const seconds = smallbreakLeft % 60;
-    pizzatimeMsg.innerText = "Pizza time - Take a break and relax :)"
+
+
+function breakDisplay() {
+    if (sessions === 5) {
+    const minutes = Math.floor(longbreakLeft / 60);
+    const seconds = longbreakLeft % 60;
+    pizzatimeMsg.innerText = "Enjoy this long break. Take a walk or something :)"
     timer_container.appendChild(pizzatimeMsg)
     timer_display.textContent = 
         `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    } 
+    else {
+        const minutes = Math.floor(smallbreakLeft / 60);
+        const seconds = smallbreakLeft % 60;
+        pizzatimeMsg.innerText = "Pizza time - Take a break and relax :)"
+        timer_container.appendChild(pizzatimeMsg)
+        timer_display.textContent = 
+            `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    }
 }
-
 
 
 function startTimer() {
@@ -37,12 +50,19 @@ function startTimer() {
             if (timeLeft > 0) {
                 timeLeft--;
                 updateDisplay();
-                sessions++
+
                 }
             else {
                 clearInterval(timer)
                 isRunning = false;
+                sessions += 1
+                if (sessions === 5) {
+                    longbreakTimer()
+                    sessions = 0
+                }
+                else {
                 breakTimer()
+                }
             }
                
         }, 1000);
@@ -50,20 +70,43 @@ function startTimer() {
 }
 
 function breakTimer() {
-        breakStart = true;
         timer = setInterval(() => {
             if (smallbreakLeft > 0) {
                 smallbreakLeft--;
-                smallbreakDisplay();
+                breakDisplay();
             } else {
-                clearInterval(timer);
-                breakStart = false;
+                clearInterval(timer);;
+                isRunning = false
+                timeLeft = 25 * 60;
+                pizzatimeMsg.innerText = ''
+                
                
 
                 // alert("Time's up!");
             }
         }, 1000);
     }
+
+
+
+
+function longbreakTimer() {
+    timer = setInterval(() => {
+        if (longbreakLeft > 0) {
+            longbreakLeft--;
+            breakDisplay();
+        } else {
+            clearInterval(timer);
+            isRunning = false
+            timeLeft = 25 * 60;
+            pizzatimeMsg.innerText = ''
+            
+           
+
+            // alert("Time's up!");
+        }
+    }, 1000);
+}
 
 // if (sessions <= 5) {
 //     smallbreakDisplay()
